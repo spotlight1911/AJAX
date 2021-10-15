c832c31df534af29ee7d6d5d5f26dad3331307d7
studentte1-1 <test@test.com>
2021-10-15 20:47:36 +0300

    Some changes in default.php for try work

--- index.php
+++ index.php
@@ -9,20 +9,7 @@ spl_autoload_register(function ($className){
     return false;
 });
 include_once 'vendor'.DIRECTORY_SEPARATOR.'includes'.DIRECTORY_SEPARATOR.'config.php';
-core\Route::init();
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<div>
 
-</div>
-</body>
-</html>
+$users = ['name'=>'Vova', 'surname'=>'Pupko', 'way'=>'photo/ggg.gif'];
+core\Response::sendJSON($users);
+
