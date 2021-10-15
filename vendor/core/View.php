19a1610556a544473998dfda7b19e85ae74f0cc0
edorofeev89 <85448490+edorofeev89@users.noreply.github.com>
2021-10-15 19:30:41 +0300

    added View.php

--- vendor/core/View.php
+++ vendor/core/View.php
@@ -1,5 +1,20 @@
 <?php
-
+namespace core;
 class View{
+    protected $_page;
+    protected $_template = 'default';
 
+    public function __construct($page, $template = null)
+    {
+        $this->_page = $page;
+        if ($template) {
+            $this->_template = $template;
+        }
+    }
+
+    public function render(array $newsAll = [])
+    {
+        extract($newsAll);
+        include_once 'vendor' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $this->_template . '.php';
+    }
 }
