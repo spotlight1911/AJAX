ab3f82207c28ca982f91e1c6a2931d0a3fc24605
MihailExplain <unkvuzutor@gmail.com>
2021-10-25 14:31:23 +0300

    edit variable`s name

--- api.php
+++ api.php
@@ -2,5 +2,5 @@
 include_once 'autoloader.php';
 include_once 'vendor'.DIRECTORY_SEPARATOR.'includes'.DIRECTORY_SEPARATOR.'config.php';
 $usersStore = new \models\Store();
-$users = $usersStore->allUser();
-core\Response::sendJSON($users);
+$allUsers = $usersStore->allUser();
+core\Response::sendJSON($allUsers);
